b18b76b7800541930131f0e6f01c93aa970dd95e
Rishikesh Salunke <rishisalunke14@gmail.com>
2026-01-30 16:50:16 +0530

    Initial upload of website

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    flatpickr("#bookingDate", {
+        enableTime: true,
+        dateFormat: "Y-m-d h:i K",  // AM / PM
+        time_24hr: false,
+        minuteIncrement: 15,
+        minDate: "today",
+        minTime: "10:00",
+        maxTime: "23:60",
+        allowInput: false
+    });
+});
+
+
+
